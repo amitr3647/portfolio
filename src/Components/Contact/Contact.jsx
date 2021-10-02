7dca1826e8236ebf9c181e3879118c33d3b4e1c3
amitr3647 <amitr3647@gmail.com>
2021-10-03 03:20:00 +0530

    making contact page

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,10 +1,48 @@
-export default function Contact(){
-    return(
-<div className="contact">
-
-<h1>this is contact page</h1>
-
-</div>
-
-    )
+import "./Contact.css";
+import logo from "../../Assets/about.png";
+import { SocialIcon } from "react-social-icons";
+export default function Contact() {
+  return (
+    <div className="contact">
+      <div className="contact-details">
+        <div className="left-container">
+          <img src={logo}></img>
+        </div>
+        <div className="right-container">
+          <div className="bio-text">
+            <h1>Reach out to me!</h1>
+            <p>
+              Discuss a project or just want to say hi? My inbox is open for
+              all.
+            </p>
+            <strong>
+              Front-end Developer👨‍💻|Open source contributor🔥|Tech Writer✍️
+            </strong>
+          </div>
+          <div className="location" style={{ color: "blueviolet " }}>
+            <ion-icon name="location-outline"></ion-icon>Patna,India
+          </div>
+          <div className="opportunity">Open for opportunities: Yes</div>
+          <div className="social-links">
+            <SocialIcon
+              className="linkedin social-icon"
+              url="https://www.linkedin.com/in/amit-kumar-6a5aa7118/"
+            />
+            <SocialIcon
+              className="twitter social-icon"
+              url="https://twitter.com/Lucifer52912819"
+            />
+            <SocialIcon
+              className="facebook social-icon"
+              url="https://www.facebook.com/profile.php?id=100003970472447"
+            />
+            <SocialIcon
+              className="github social-icon"
+              url="https://github.com/amitr3647"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
